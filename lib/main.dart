f1a062d0edb34e0d67001be44d27957ded601046
Seungchul Ha <seungchulha@Seungchuls-MacBook-Pro.local>
2023-04-24 13:42:20 +0900

    Buttons & Icons

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,12 @@ class Home extends StatelessWidget {
         backgroundColor: Colors.red[600],
       ),
       body: Center(
-        child: Image.network(
-            'https://i.natgeofe.com/k/5b396b5e-59e7-43a6-9448-708125549aa1/new-york-statue-of-liberty_2x1.jpg'),
+        child: IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.alternate_email),
+          color: Colors.red,
+          splashColor: Colors.amber,
+        ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {},
@@ -28,4 +32,54 @@ class Home extends StatelessWidget {
       ),
     );
   }
-}
+} 
+
+// Icon(
+//   Icons.airport_shuttle, // 원하는 아이콘 모양
+//   color = Colors.lightBlue, // 아이콘의 색상
+//   size = 50.0, // 아이콘의 사이즈
+// )
+
+// ElevatedButton(
+//   onPressed: () {},
+//   style: ElevatedButton.styleFrom(
+//     foregroundColor: Colors.amber,
+//     backgroundColor: Colors.black,
+//   ),
+//   child: const Text('click me'),
+// )
+
+// TextButton(
+//   onPressed: () {},
+//   style: TextButton.styleFrom(
+//     foregroundColor: Colors.redAccent,
+//     textStyle: const TextStyle(
+//       fontFamily: 'IndieFlower',
+//       fontSize: 80.0,
+//       fontStyle: FontStyle.italic,
+//     ),
+//   ),
+//   child: const Text('click me'),
+// )
+
+// ElevatedButton.icon(
+//   onPressed: () {},
+//   style: ElevatedButton.styleFrom(
+//     backgroundColor: Colors.amber,
+//   ),
+//   icon: const Icon(Icons.mail),
+//   label: const Text('Mail Me'),
+// )
+
+// IconButton(
+//   onPressed: () {},
+//   icon: const Icon(Icons.alternate_email),
+//   color: Colors.red,
+//   splashColor: Colors.amber,
+// )
+
+// OutlinedButton(
+//   onPressed: () {},
+//   style: OutlinedButton.styleFrom(shape: const StadiumBorder()),
+//   child: const Text('Hello'),
+// )
