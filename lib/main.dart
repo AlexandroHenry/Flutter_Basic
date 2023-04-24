947f1239a92363b8913151e2ebb8c81a2bd320ea
Seungchul Ha <seungchulha@Seungchuls-MacBook-Pro.local>
2023-04-25 00:01:38 +0900

    Lists of Data

--- lib/main.dart
+++ lib/main.dart
@@ -2,141 +2,52 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MaterialApp(
-    home: SRCard(),
+    home: QuoteList(),
   ));
 }
 
-class SRCard extends StatefulWidget {
-  const SRCard({super.key});
+class QuoteList extends StatefulWidget {
+  const QuoteList({super.key});
 
   @override
-  State<SRCard> createState() => _SRCardState();
+  State<QuoteList> createState() => _QuoteListState();
 }
 
-class _SRCardState extends State<SRCard> {
-  int cellMember = 0;
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'Be yourself; everyone else is already taken',
+    'I have nothing to declare except my genius',
+    'The truth is rarely pure and never simple',
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.black,
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: const Text('SR ID Card'),
-        centerTitle: true,
+        title: const Text(
+          'Awesome Quotes',
+          style: TextStyle(
+            fontFamily: 'IndieFlower',
+            fontSize: 30,
+            color: Colors.amber,
+          ),
+        ),
         backgroundColor: Colors.red[600],
-        elevation: 0.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          setState(() {
-            cellMember += 1;
-          });
-        },
-        backgroundColor: Colors.red,
-        child: const Icon(
-          Icons.add,
-        ),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.fromLTRB(30, 40, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Center(
-              child: CircleAvatar(
-                backgroundImage: AssetImage('assets/chunsik1.png'),
-                radius: 100,
-              ),
-            ),
-            const Divider(
-              height: 90,
-              color: Colors.grey,
-            ),
-            const Text(
-              'NAME',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              'Sungrak',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                letterSpacing: 2.0,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30.0,
-            ),
-            const Text(
-              'CURRENT TEAM',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            const Text(
-              'J-ON',
-              style: TextStyle(
-                color: Colors.amberAccent,
-                letterSpacing: 2.0,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Text(
-              'Total Cell',
-              style: TextStyle(
-                color: Colors.white,
-                letterSpacing: 2.0,
-              ),
-            ),
-            const SizedBox(
-              height: 10.0,
-            ),
-            Text(
-              '$cellMember',
-              style: const TextStyle(
-                color: Colors.amberAccent,
-                letterSpacing: 2.0,
-                fontSize: 28,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Row(
-              children: const [
-                Icon(
-                  Icons.email,
-                  color: Colors.amber,
-                ),
-                SizedBox(width: 15),
-                Text(
-                  'alaxhenry@gmail.com',
-                  style: TextStyle(
-                    color: Colors.amber,
-                    fontSize: 18,
-                    letterSpacing: 1,
+      body: Column(
+        children: quotes
+            .map((quote) => Text(
+                  quote,
+                  style: const TextStyle(
+                    fontFamily: 'IndieFlower',
+                    fontSize: 30,
+                    color: Colors.lightBlueAccent,
+                    letterSpacing: 3,
+                    wordSpacing: 3,
                   ),
-                ),
-              ],
-            ),
-          ],
-        ),
+                ))
+            .toList(),
       ),
     );
   }
